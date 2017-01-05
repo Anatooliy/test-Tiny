20549e38bd9a3a50e9231951469958786784ce44
Anatooliy <anatoly@spb.mcart.ru>
2017-01-05 23:17:11 +0200

    add progect

---
+++ testTiny/testTiny/Controllers/TinyController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using testTiny.Models;
+
+namespace testTiny.Controllers
+{
+    public class TinyController : Controller
+    {
+        // GET: Tiny
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(TinyModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ testTiny/testTiny/Models/TinyModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace testTiny.Models
+{
+    public class TinyModel
+    {       
+        [AllowHtml]
+        public string HtmlContent { get; set; }
+
+        public TinyModel()
+        {
+
+        }
+    }
+}
